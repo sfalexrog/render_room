359bd60b0f27b9f0fae05b13133a05fa4fe84537
sfalexrog <sfalexrog@gmail.com>
2017-11-30 22:07:08 +0300

    Add empty composer files to satisfy cmake
    They are on a TODO list

---
+++ src/render/composer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sf on 28/11/17.
+//
+
+#include "composer.h"
---
+++ src/render/composer.h
@@ -0,0 +1,8 @@
+//
+// Created by sf on 28/11/17.
+//
+
+#ifndef CMC_RAYMARCH_COMPOSER_H
+#define CMC_RAYMARCH_COMPOSER_H
+
+#endif //CMC_RAYMARCH_COMPOSER_H
